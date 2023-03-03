d68ce0d972ed0eadd95c0e4db718e0af62fe62b6
Thiago Cardoso <cafeprathi@gmail.com>
2023-03-02 23:17:20 -0300

    init

--- project_buscapreco/app_buscapreco/views.py
+++ project_buscapreco/app_buscapreco/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import Produto
 
-# Create your views here.
 
 def pesquisar(request):
     return render(request, 'produtos/pesquisa.html')
 
 
-def exibir_resultados():
-    pass
+def exibir_resultados(request):
+    nome_produto = request.POST.get('produto')
+    print(nome_produto)
+    dados = { 'dados': Produto.objects.filter(name__icontains=nome_produto).order_by('price')}
+    return render(request, 'produtos/resultado_pesquisa.html', dados)
